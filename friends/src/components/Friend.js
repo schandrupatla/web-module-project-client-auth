ec4992970d74009ddc28f0b4a4b5a415bea60050
schandrupatla <55516943+schandrupatla@users.noreply.github.com>
2021-05-18 19:34:13 -0700

    cleaned friend component code

--- friends/src/components/Friend.js
+++ friends/src/components/Friend.js
@@ -5,13 +5,13 @@ const Friend =(props)=>{
      const {friend} = props;
 
     return (
-          <div className="Friend">
+    <div className="Friend">
       <header className="App-header">
-        <h1>Welcome to Sridevi's friend's page</h1>
+        {/* <h1>Welcome to Sridevi's friend's page</h1> */}
         <div className="card">
-        <h3>Name={friend.name}</h3>
-        <h3>Age={friend.age}</h3>
-        <h3>Email={friend.email}</h3>
+        <h3>{friend.name}</h3>
+        <h3>{friend.age}</h3>
+        <h3>{friend.email}</h3>
         </div>
         
       </header>
